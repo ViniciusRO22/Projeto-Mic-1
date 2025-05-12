b702ef03f1b52219e573147973835d274a3a516c
Vinicius R.O <ramalhooliveiravini@gmail.com>
2025-05-12 20:43:54 -0300

    Subindo ULA 1 bit correto e ULA 8 bits

---
+++ simulation/qsim/work/@u@l@a_8bit_vlg_check_tst/_primary.vhd
@@ -0,0 +1,8 @@
+library verilog;
+use verilog.vl_types.all;
+entity ULA_8bit_vlg_check_tst is
+    port(
+        COUT            : in     vl_logic;
+        sampler_rx      : in     vl_logic
+    );
+end ULA_8bit_vlg_check_tst;
